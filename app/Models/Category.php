ea14d7ad6d61c2d5fd08a197e5c1aca0f7e4599a
Rauf <raufkhandevs@gmail.com>
2025-03-02 13:54:33 +0500

    generate seeders

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
+    use HasFactory;
+
+    const DEFAULT_CATEGORIES = [
+        'technology',
+        'business',
+        'sports',
+        'entertainment',
+        'health',
+        'science',
+        'politics',
+        'general',
+    ];
+
     protected $fillable = [
         'name',
         'slug',
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->createAdmin();
+    }
+
+    private function createAdmin()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'), // TODO: Change this to a more secure password
+            'is_admin' => true,
+        ]);
+    }
+}
