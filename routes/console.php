c723fe9d6bd4d1fff3c4712fc2e2ff9ef505861b
Rauf <raufkhandevs@gmail.com>
2025-03-02 17:47:46 +0500

    running cron job using sail

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\FetchArticlesCommand;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(FetchArticlesCommand::class)->hourly();
